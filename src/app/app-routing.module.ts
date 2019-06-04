5cba5298c0bed8c0d80f73186911217991c05ab0
Mason Turbyfill <masonsturbyfill61796@gmail.com>
2019-06-04 14:49:40 -0400

    Inital setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PokedexComponent } from './pokedex/pokedex.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/pokedex', pathMatch: 'full' },
+  { path: 'pokedex', component: PokedexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokedexComponent
   ],
   imports: [
     BrowserModule,
